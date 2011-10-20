92c5dcd6814d40ca5127f58bd38c2ef75e9f5019
Ramana Malladi <vmallad@ncsu.edu>
2011-10-19 21:11:04 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mysecapp::Application.config.secret_token = 'd4e4cb309db6bab562ae2676b0f25cc24fc3eda1e3adeb9e0b4006122056c9bd73ae4f4726d427b535c56ecc43106dcbf22719b9edd28ec648d938f7ef90dd4e'
